9fafab21bf173c986004ab612338dcce5f98ea36
capitanpikachucf <capitanpikachucf@gmail.com>
2025-05-05 12:35:09 -0500

    v1

---
+++ capitanpikachucf/src/main/java/com/capitanpikachucf/capitanpikachucf/CapitanpikachucfApplication.java
@@ -0,0 +1,13 @@
+package com.capitanpikachucf.capitanpikachucf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapitanpikachucfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapitanpikachucfApplication.class, args);
+	}
+
+}
---
+++ capitanpikachucf/src/test/java/com/capitanpikachucf/capitanpikachucf/CapitanpikachucfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capitanpikachucf.capitanpikachucf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapitanpikachucfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
